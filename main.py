73713176e2b9792460af2d10f6ae9f9ad430ffe8
Jack <jackcshin@gmail.com>
2022-12-11 20:01:22 -0800

    1st commit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-num_inputs = []
-for i in range(10):
-    num = int(input("Enter a number: "))
-    num_inputs.append(num)
-avg = sum(num_inputs)/10
-for i in num_inputs:
+
+num = list(map(int,input("Enter a number: ").split()))
+avg = sum(num)/10
+for i in num:
 
     print(f'{abs(avg-i):.2f}', end=" ")
